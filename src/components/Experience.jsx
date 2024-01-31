e3caae45fa6e3e950120152d53610d633f8745af
Ben <bencampbell90@outlook.com>
2024-01-31 20:50:21 +1100

    add new link and functionality

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,5 @@
 /* eslint-disable react/prop-types */
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import { motion } from 'framer-motion';
 
 import 'react-vertical-timeline-component/style.min.css';
@@ -12,14 +9,7 @@ import { experiences } from '../constants';
 import SectionWrapper from '../hoc';
 import { textVariant } from '../utils/motion';
 
-const ExperienceCard = ({
-  date,
-  iconBg,
-  icon,
-  title,
-  company_name,
-  points,
-}) => {
+const ExperienceCard = ({ date, iconBg, icon, title, company_name, points }) => {
   return (
     <VerticalTimelineElement
       contentStyle={{ background: '#1d1836', color: '#fff' }}
@@ -28,27 +18,18 @@ const ExperienceCard = ({
       iconStyle={{ background: iconBg }}
       icon={
         <div className="flex justify-center items-center w-full h-full">
-          <img
-            src={icon}
-            alt={company_name}
-            className="w-[60%] h-[60%] object-contain"
-          />
+          <img src={icon} alt={company_name} className="w-[60%] h-[60%] object-contain" />
         </div>
       }
     >
       <div>
         <h3 className="text-white text-[24px] font-bold">{title}</h3>
-        <p className="text-secondary text-[16px] font-semibold m-0">
-          {company_name}
-        </p>
+        <p className="text-secondary text-[16px] font-semibold m-0">{company_name}</p>
       </div>
 
       <ul className="mt-5 list-disc ml-5 space-y-2">
         {points.map((point, index) => (
-          <li
-            key={`experience-point-${index}`}
-            className="text-white-100 text-[14px] pl-1 tracking-wider"
-          >
+          <li key={`experience-point-${index}`} className="text-white-100 text-[14px] pl-1 tracking-wider">
             {point}
           </li>
         ))}
